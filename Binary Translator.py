a5fd6a31c3f5c6698ef7e764870f5d4871e8de62
karencita <noeliatareascole@gmail.com>
2025-07-28 20:51:38 -0500

    Update Binary Translator.py

--- Binary Translator.py
+++ Binary Translator.py
@@ -6,7 +6,7 @@ for letrita in textito:
     bin_8bits = format(ascii_num, "08b")
     binarios.append(bin_8bits)
 
-binario_final = ""
+binario_final = " "
 for x in binarios:
     binario_final += x + " "
 binario_final = binario_final.strip()
